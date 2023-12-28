5c272203f57e48463b9cf68c533b4cbc7e2dc286
TanaT <mihail.zubko@yandex.ru>
2023-12-29 01:06:18 +0300

    added dict 'sounds' moved sounds from Fighter property to dict 'sounds'

---
+++ sounds.py
@@ -0,0 +1,9 @@
+import pygame
+
+sounds = {
+    'fighter': {
+        'punch': pygame.mixer.Sound('sounds/punch.mp3'),
+        'jump': pygame.mixer.Sound('sounds/jump.mp3'),
+        'kick': pygame.mixer.Sound('sounds/kick.mp3')
+    }
+}
